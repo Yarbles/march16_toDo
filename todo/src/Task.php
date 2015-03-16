4e80b9908938c410642a5c3209482dde8140d862
Tanner Heffner & Reid Baldwin <student@epicodus.com>
2015-03-16 10:35:26 -0700

    Change all  variables to ['DB'], add tests for save & delete

--- todo/src/Task.php
+++ todo/src/Task.php
@@ -7,26 +7,37 @@ class Task
     {
         $this->description = $description;
     }
+
     function setDescription($new_description)
     {
         $this->description = (string) $new_description;
     }
+
     function getDescription()
     {
         return $this->description;
     }
+
     function save()
     {
-        array_push($_SESSION['list_of_tasks'], $this);
+        $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
     }
+
     static function getAll()
     {
-        return $_SESSION['list_of_tasks'];
+        $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+        $tasks = array();
+        foreach($returned_tasks as $task) {
+            $description = $task['description'];
+            $new_task = new Task($description);
+            array_push($tasks, $new_task);
+        }
+        return $tasks;
     }
 
     static function deleteAll()
     {
-        $_SESSION['list_of_tasks'] = array();
+        $GLOBALS['DB']->exec("DELETE FROM tasks *;");
     }
 
 }
