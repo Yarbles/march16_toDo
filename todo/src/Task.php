03883a48dcfacdcb914cb76f39bc88a2db3c5ed4
Tanner Heffner & Reid Baldwin <student@epicodus.com>
2015-03-16 11:09:39 -0700

    Add  to all instances of task class

--- todo/src/Task.php
+++ todo/src/Task.php
@@ -2,10 +2,14 @@
 class Task
 {
     private $description;
+    private $id;
 
-    function __construct($description)
+    function __construct($description, $id)
     {
         $this->description = $description;
+        if($id !== null) {
+            $this->id = $id;
+        }
     }
 
     function setDescription($new_description)
@@ -18,9 +22,21 @@ class Task
         return $this->description;
     }
 
+    function getId()
+    {
+        return $this->id;
+    }
+
+    function setId($new_id)
+    {
+        $this->id = (int) $new_id;
+    }
+
     function save()
     {
-        $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+        $statement = $GLOBALS['DB']->query("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}') RETURNING id;");
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        $this->setId($result['id']);
     }
 
     static function getAll()
@@ -29,7 +45,8 @@ class Task
         $tasks = array();
         foreach($returned_tasks as $task) {
             $description = $task['description'];
-            $new_task = new Task($description);
+            $id = $task['id'];
+            $new_task = new Task($description, $id);
             array_push($tasks, $new_task);
         }
         return $tasks;
